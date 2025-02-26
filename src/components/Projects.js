d315e269556d2bcfe0b88f4c0c77cb174897180e
bhimamalbhage <bhimamalbhage@hotmail.com>
2025-02-25 19:36:25 -0600

    added a project item

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import AiAssistant from "../assets/images/ai-assistant.png";
-import Traffic from '../assets/images/traffic.png';
-import eventManagement from '../assets/images/event-management.jpg';
+import myFlix from "../assets/images/myFlix.png";
+import Traffic from "../assets/images/traffic.png";
+import eventManagement from "../assets/images/event-management.jpg";
 
 const projects = [
   {
     title: "Custom AI-powered chatbot for university services",
     image: AiAssistant,
     githubLink: "https://github.com/bhimamalbhage/AI-Assistant-for-university",
+  },
+  {
+    title: "MyFlix - Movie Discovery and Management App",
+    image: myFlix,
+    githubLink: "https://github.com/bhimamalbhage/AI-Assistant-for-university",
+    websiteLink: 'https://myflix-bhima.netlify.app/',
   },
   {
     title: "Scalable traffic data sensing systems using AI",
@@ -25,31 +33,47 @@ const projects = [
 
 const Project = () => {
   return (
-      <div className="space-y-12">
-        <h2 className="text-3xl font-semibold text-gray-800">Projects</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 mt-8">
-          {projects.map((project, index) => (
-            <div key={index} className="group overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-xl transition-all duration-300">
-              <img
-                src={project.image}
-                alt={project.title}
-                className="w-full h-56 object-cover group-hover:scale-105 transition-transform duration-500"
-              />
-              <div className="p-6">
-                <h3 className="text-xl font-semibold text-gray-700">{project.title}</h3>
+    <div className="space-y-12">
+      <h2 className="text-3xl font-semibold text-gray-800">Projects</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 mt-8">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className="group overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-xl transition-all duration-300"
+          >
+            <img
+              src={project.image}
+              alt={project.title}
+              className="w-full h-56 object-cover group-hover:scale-105 transition-transform duration-500"
+            />
+            <div className="p-6">
+              <h3 className="text-xl font-semibold text-gray-700">
+                {project.title}
+              </h3>
+              <a
+                href={project.githubLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-3 inline-block"
+              >
+                <FontAwesomeIcon icon={faGithub} className="w-5 h-5" />
+              </a>
+              {project.websiteLink && (
                 <a
-                  href={project.githubLink}
+                  href={project.websiteLink}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mt-3 inline-block"
+                  style={{ marginLeft: "10px" }}
                 >
-                 <FontAwesomeIcon icon={faGithub} className="w-5 h-5" />
+                  <FontAwesomeIcon icon={faExternalLinkAlt} className="w-5 h-5" />
                 </a>
-              </div>
+              )}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
+    </div>
   );
 };
 
